a408df44b8344c0a9e247b93d9a49cadf13021ab
zakki_zamani <dezzp1995@gmail.com>
2024-12-17 15:55:34 +0700

    change config and import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+'use client'
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+import styles from './ui/home.module.css';
 import {lusitana} from '@/app/ui/fonts';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
   return (
